f990a077dd7524628a13c34b84938e97919ff623
allanolive <allan.loveforfood@gmail.com>
2019-04-16 11:57:45 -0400

    Created Home component
    - with some placeholder text

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+const Home = () => (
+  <Fragment>
+    <h2>Mio Pomodoro</h2>
+    <p>Mama Mia, it&#39;s deliciously</p>
+  </Fragment>
+)
+
+export default Home
