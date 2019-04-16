bc56bd529421247dc3be8da27f9c4b50bd7e40b8
allanolive <allan.loveforfood@gmail.com>
2019-04-16 11:48:40 -0400

    Create axios requests
    - create getPomodoros request INDEX
    - create getPomodoro request SHOW
    - create deletePomodoro request DELETE
    - create createPomodoro request POST
    - create editPomodoro request PATCH

--- src/auth/api.js
+++ src/auth/api.js
@@ -53,3 +53,61 @@ export const changePassword = (passwords, user) => {
     }
   })
 }
+
+export const getPomodoros = user => {
+  return axios({
+    url: apiUrl + `/users/${user.id}`,
+    method: 'GET',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+
+export const getPomodoro = (user, id) => {
+  return axios({
+    url: apiUrl + '/pomodoros/' + id,
+    method: 'GET',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+
+export const deletePomodoro = (user, id) => {
+  return axios({
+    url: apiUrl + '/pomodoros/' + id,
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+
+export const createPomodoro = (pomodoro, token) => {
+  return axios({
+    url: `${apiUrl}/pomodoros`,
+    method: 'POST',
+    data: { pomodoro: {
+      work_time: pomodoro.workTime,
+      break_time: pomodoro.breakTime
+    } },
+    headers: {
+      'Authorization': `Token token=${token}`
+    }
+  })
+}
+
+export const editPomodoro = (pomodoro, token) => {
+  return axios({
+    url: `${apiUrl}/pomodoros/${pomodoro.id}`,
+    method: 'patch',
+    data: { pomodoro: {
+      work_time: pomodoro.workTime,
+      break_time: pomodoro.breakTime
+    } },
+    headers: {
+      'Authorization': `Token token=${token}`
+    }
+  })
+}
