f34818ccc11e2148773e21285aec2d43570c943c
allanolive <allan.loveforfood@gmail.com>
2019-04-19 00:24:45 -0400

    Created custom messages for CRUD

--- src/auth/messages.js
+++ src/auth/messages.js
@@ -5,6 +5,10 @@ export default {
   signInSuccess: 'Welcome!',
   signInFailure: 'Failed to sign in. Check your email and password and try' +
                   ' again.',
+  createPomodoroSuccess: 'You have created a NEW pomodoro!',
+  createPomodoroFailure: 'You have failed to create a new pomodoro!',
+  deletePomodoroSuccess: 'You have successfully deleted your pomodoro!',
+  updatedPomodoroSuccess: 'You have successfully updated your pomodoro!',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
   changePasswordFailure: 'Failed to change passwords. Check your old password' +
